5f85c5f71d658a5c4b9459ebae987ecee7aa92e1
Camberden <42500247+Camberden@users.noreply.github.com>
2024-10-06 21:43:06 -0400

    calc play

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,22 @@ fn hi(){
     for i in 0..10 {
         println!("I am {} years old", i);
     }
+
+    println!("{}", calculator(5, 5, "+".to_string()));
 }
 
+fn calculator(s: i64, p: i64, t: String) -> i64 {
+    println!("{}{}{}", s, t, p );
+    let result = match t.as_str() {
+        "+" => s + p,
+        "-" => s - p,
+        "*" => s * p,
+        "/" => s / p,
+        _ => panic!(),
+    };
+    result
+}
 
-
-
+const ss: &str = "SuperString";
 
 
